8d075ddbb1e33f5d3c51183b66d0bff03a9d9bd7
kpolimis <kivan.polimis@gmail.com>
2021-03-14 01:01:50 -0500

    modified utils and 01_case_adjustment_plots.R with more documentation and renamed associated plots

--- code/01_case_adjustment_plots.R
+++ code/01_case_adjustment_plots.R
@@ -1,34 +1,26 @@
 #' model original and adjusted covid case positivity rates using data from https://covidtracking.com/
 #' https://covidtracking.com/ was discontinued 7th March 2021
-#' the positivity adjustment formula is courtesy of http://freerangestats.info see following links
+#' 
+#' the test positivity rate adjustment formula is courtesy of http://freerangestats.info see following links for additional information:
 #' http://freerangestats.info/blog/2020/05/09/covid-population-incidence
 #' http://freerangestats.info/blog/2020/05/17/covid-texas-incidence
 #' https://github.com/ellisp/blog-source/blob/master/_working/0178-covid-prevalence-inference.R
 
-# install.packages(c("tidyverse", "janitor", "scales", "Cairo", "mgcv", "EpiEstim", "patchwork", "lubridate", "ggrepl", "ggseas"), dependencies = TRUE)
-# devtools::install_github("ellisp/frs-r-package/pkg")
-# remotes::install_github(c("ropensci/tabulizerjars", "ropensci/tabulizer"))
+# install.packages(c("here", "mgcv", "ggseas", "scales", "janitor", "lubridate", "tidyverse"), dependencies = TRUE)
 
 library(here)
-library(tidyverse)
-library(janitor)
-library(scales)
-library(Cairo)
 library(mgcv)
-library(EpiEstim)
-library(patchwork)
-library(frs)
-library(tabulizer)
-library(lubridate)
-library(ggrepel)
 library(ggseas)
-# CairoWin()
+library(scales)
+library(janitor)
+library(lubridate)
+library(tidyverse)
 
 source(here("code/utils.R"))
 
 #' import data
 
-#' covid case data by state
+#' covid-19 case data by state
 covid_tracking_state_data_raw = read_csv("https://covidtracking.com/api/v1/states/daily.csv") 
 covid_tracking_state_info = read_csv("https://covidtracking.com/api/v1/states/info.csv")
 
@@ -44,7 +36,7 @@ covid_tracking_state_info = covid_tracking_state_info %>% rename("state_abb"="st
 unique(covid_tracking_state_data_raw$state_abb)
 unique(covid_tracking_state_info$state_abb)
 
-#' population by state
+#' 2019 population by state
 census_2019_state_pop_url = "https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/state/detail/SCPRC-EST2019-18+POP-RES.csv"
 census_2019_state_pop = read_csv(census_2019_state_pop_url)
 
@@ -70,7 +62,7 @@ covid_tracking_state_data = covid_tracking_state_data_raw %>%
   clean_names() %>%
   #' force total number of tests to be at least as many as the number of positives:
   mutate(total_test_results_increase = pmax(positive_increase, total_test_results_increase)) %>%
-  mutate(pos_rate = positive_increase / total_test_results_increase) %>%
+  mutate(test_positivity_rate = positive_increase/total_test_results_increase) %>%
   arrange(date) %>%
   mutate(date_n = as.numeric(date))  %>%
   left_join(select(covid_tracking_state_info, state_abb, state_name = name), by = "state_abb")
@@ -80,15 +72,15 @@ length(sort(unique(covid_tracking_state_data$state_abb)))
 names(covid_tracking_state_data)
 
 #' just the 12 biggest states
-top_12_populous_states_case_data = covid_tracking_state_data %>%
+top_12_populous_states_testing_data = covid_tracking_state_data %>%
   group_by(state_abb) %>%
-  dplyr::summarise(max_pos = max(positive)) %>%
+  summarise(max_pos = max(positive)) %>%
   arrange(desc(max_pos)) %>%
   filter(state_abb %in% top_12_populous_states$state_abb) %>%
   inner_join(covid_tracking_state_data, by = "state_abb") %>%
   #' state has to be a factor for use in mgcv::gam:
   mutate(state_name = fct_reorder(state_name, positive, .fun = sum),
-         pos_rate = ifelse(pos_rate<0, 0, pos_rate)) %>%
+         test_positivity_rate = ifelse(test_positivity_rate<0, 0, test_positivity_rate)) %>%
   #' we want deaths in 7-14 days time as a crude indicator of cases now, for use later
   #' Tried various methods and 7 was best. Obviously, if doing this 'for real', 7 should
   #' be a parameter we estimate from the data
@@ -97,49 +89,54 @@ top_12_populous_states_case_data = covid_tracking_state_data %>%
   mutate(deaths_x_days_later = lead(death_increase, 7)) %>%
   ungroup()
 
-sort(unique(top_12_populous_states_case_data$state_abb))
+sort(unique(top_12_populous_states_testing_data$state_abb))
 
-#' smooth the positive test rates
-top_12_populous_states_mod = gam(pos_rate ~ state_name + s(date_n, by = state_name), 
-                    data = top_12_populous_states_case_data, 
-                    family = quasibinomial,
-                    weights = total_test_results_increase)
+#' create a model to smooth the positive test rates
+top_12_populous_states_model = gam(test_positivity_rate ~ state_name + s(date_n, by = state_name), 
+                                   data = top_12_populous_states_testing_data, 
+                                   family = quasibinomial,
+                                   weights = total_test_results_increase)
 
-top_12_populous_states_plot_data = top_12_populous_states_case_data %>%
-  generate_smooth_data(., model=top_12_populous_states_mod)
+#' apply smoothed model and adjust the test positivity rates for the top 12 most populous states and texas
+adjusted_populous_states_testing_data = top_12_populous_states_testing_data %>%
+  adjust_test_positivity_data(., model=top_12_populous_states_model)
 
-texas_plot_data = top_12_populous_states_case_data %>%
+adjusted_texas_testing_data = top_12_populous_states_testing_data %>%
   filter(state_name == "Texas") %>%
-  generate_smooth_data(., model=top_12_populous_states_mod)
+  adjust_test_positivity_data(., model=top_12_populous_states_model)
 
-top_12_populous_states_case_adjustment_plot = top_12_populous_states_plot_data %>%
+#' create plots for observed and adjusted testing positivity rates by top 12 populous states and texas
+observed_adjusted_populous_states_testing_plot = adjusted_populous_states_testing_data %>%
   ggplot(aes(x = date, y = value, colour = variable)) +
   facet_wrap(~state_name, scale = "free_y")  +
   stat_rollapplyr(index.ref = 60, width = 7) +
-  the_theme +
-  the_labs +
+  adjustment_theme +
+  adjustment_labels +
   scale_colour_brewer(palette = "Set1") +
-  ggtitle("Trends in daily COVID-19 cases (rolling seven-day average, scale-free index).
-           With and without case adjustment for test positivity rate")
+  scale_y_continuous(label=comma) +
+  ggtitle("Trends in Top 12 Populous States Observed and Adjusted Daily COVID-19 Test Positivity Rate")
 
 #' plot suggests relatively more unknown cases in March and April
 
-texas_case_adjustment_plot = texas_plot_data %>%
+observed_adjusted_texas_testing_plot = adjusted_texas_testing_data %>%
   ggplot(aes(x = date, y = value, colour = variable)) +
   stat_rollapplyr(index.ref = 70, width = 7) +
-  the_theme +
-  the_labs +
+  adjustment_theme +
+  adjustment_labels +
   scale_colour_brewer(palette = "Set1") +
-  ggtitle("Trends in daily COVID-19 cases in Texas (rolling seven-day average, scale-free index). 
-          With and without case adjustment for test positivity rate")
+  scale_y_continuous(label=comma) +
+  ggtitle("Trends in Texas' Observed and Adjusted Daily COVID-19 Test Positivity Rate")
 
 #' after adjustment for test-positivity, plot suggests new cases are still accelerating
 
+#' make folders to save .pngs and .RData 
 ifelse(!dir.exists(file.path(here("output"))), dir.create(file.path(here("output"))), FALSE)
 ifelse(!dir.exists(file.path(here("rdata"))), dir.create(file.path(here("rdata"))), FALSE)
 
-ggsave(here("output/top_12_populous_states_case_adjustment_plot.png"),
-       top_12_populous_states_case_adjustment_plot, width=10.67, height=6, dpi=120)
+ggsave(here("output/observed_adjusted_populous_states_testing_plot.png"),
+       observed_adjusted_populous_states_testing_plot, width=10.67, height=6, dpi=120)
 
-ggsave(here("output/texas_case_adjustment_plot.png"),
-       texas_case_adjustment_plot, width=10.67, height=6, dpi=120)
+ggsave(here("output/observed_adjusted_texas_testing_plot.png"),
+       observed_adjusted_texas_testing_plot, width=10.67, height=6, dpi=120)
+
+save.image(here("rdata/adjust_test_positivity_rates.RData"), compress = TRUE)
--- code/utils.R
+++ code/utils.R
@@ -1,28 +1,34 @@
-generate_smooth_data = function(state_df, model){
-  state_df[["pos_rate_smoothed"]] = predict(model, newdata = state_df, type = "response")
-  state_df = state_df %>%
-  mutate(adj_pos = positive_increase * sqrt(pos_rate_smoothed)) %>%
-  select(date, state_name, positive_increase, adj_pos) %>%
-  gather(variable, value, -date, -state_name) %>%
-  mutate(variable = if_else(variable == "adj_pos",
-                            true = "Adjusted for test positivity rate",
-                            false = "Original")) %>%
-  group_by(state_name, variable) %>%
-  arrange(date) %>%
-  ungroup() %>%
-  mutate(value = value / value[n()] * 100)
-  return(state_df)
+adjust_test_positivity_data = function(test_positivity_data, model){
+  #' function to adjust test positivity data with a gam model of smoothed test positivity rates
+  #' 
+  #' @param test_positivity_data dataframe of test positivity data aggregated at a geographic unit (e.g., states)
+  #' @param model gam model to smooth test positivity rates
+  #' @return adjusted_test_positivity_data dataframe of observed and adjusted test positivity data
+  test_positivity_data[["smoothed_positivity_rate"]] = predict(model, newdata = test_positivity_data, type = "response")
+  adjusted_test_positivity_data = test_positivity_data %>%
+    mutate(adjusted_test_positivity_rate = positive_increase * sqrt(smoothed_positivity_rate)) %>%
+    select(date, state_name, positive_increase, adjusted_test_positivity_rate) %>%
+    gather(variable, value, -date, -state_name) %>%
+    mutate(variable = if_else(variable == "adjusted_test_positivity_rate",
+                              true = "Adjusted Test Positivity Rate",
+                              false = "Observed Test Positivity Rate")) %>%
+    group_by(state_name, variable) %>%
+    arrange(date) %>%
+    ungroup() %>%
+    mutate(value = value / value[n()] * 100)
+
+  return(adjusted_test_positivity_data)
 }  
 
-#-----------------Common themes and labels-------
+#' adjustment themes and label
 
-the_theme = theme(axis.text.y = element_blank(),
-                   panel.grid.major.x = element_blank(),
-                   panel.grid.minor.x = element_blank(),
-                   panel.grid.minor.y = element_blank(),
-                  legend.position="bottom")
+adjustment_theme = theme(panel.grid.major.x = element_blank(),
+                         panel.grid.minor.x = element_blank(),
+                         panel.grid.minor.y = element_blank(),
+                         legend.position = "bottom")
 
-the_labs = labs(x = "", 
-                 colour = "", 
-                 y = "New daily confirmed cases",
-                 caption = "Source: data from covidtracking.com")
+adjustment_labels = labs(x = "", 
+                         colour = "", 
+                         y = "New Daily Confirmed Cases",
+                         subtitle = "",
+                         caption = "rolling seven-day average, sourced from covidtracking.com")
